985bd1639c7b7d61b2514cb43f42c28f515a1bd6
Frida Eriksson <Frida.Eriksson@silf.se>
2020-04-26 15:19:29 +0200

    made LoadingIndicator using ui with loadingState, added to reducer in App

--- code/src/App.js
+++ code/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import { product } from "./reducers/product"
+import { ui } from "./reducers/ui"
 import { ProductInformation } from "components/ProductInformation";
+import { LoadingIndicator } from "components/LoadingIndicator";
 
 
 const reducer = combineReducers({
-  product: product.reducer
+  product: product.reducer,
+  ui: ui.reducer
 })
 
 const store = configureStore({ reducer })
@@ -14,6 +17,7 @@ const store = configureStore({ reducer })
 export const App = () => {
   return (
     <Provider store={store}>
+      <LoadingIndicator />
       <ProductInformation />
     </Provider>
   );
